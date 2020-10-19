68f1aeda57e96b1d43e895def06d055d2baa57f3
amin <singlepageprogrammer@gmail.com>
2020-10-19 18:00:19 +0330

    amin: arrays, classes, objects

--- annotations/variables.ts
+++ annotations/variables.ts
@@ -6,3 +6,19 @@ let nothing: undefined = undefined;
 
 // built in objets
 let now: Date = new Date();
+
+// Array
+let colors: string[] = ['red', 'green', 'blue'];
+let myNumbers: number[] = [1, 2, 3];
+let truths: boolean[] = [true, true, false];
+
+// Classes
+class Car {}
+
+let car: Car = new Car();
+
+// Object literal
+let point: { x: number; y: number } = {
+  x: 10,
+  y: 20,
+};
