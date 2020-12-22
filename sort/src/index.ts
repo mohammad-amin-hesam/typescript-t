8f878ee159808d0ff91b5606660cb8c7c2c04d4b
amin <singlepageprogrammer@gmail.com>
2020-12-22 18:44:14 +0330

    amin: abstract classes(90) finished

--- sort/src/index.ts
+++ sort/src/index.ts
@@ -1,8 +1,29 @@
+import { LinkedList } from './LinkedList';
 import { NumbersCollection } from './NumbersCollection';
 import { Sorter } from './Sorter';
 
 const numbersCollection = new NumbersCollection([10, 3, -5, 0]);
 
-const soter = new Sorter(numbersCollection);
-soter.sort();
-console.log(numbersCollection.data);
+// const soter = new Sorter(numbersCollection);
+// soter.sort();
+// console.log(numbersCollection.data);
+
+// const linkedList = new LinkedList();
+// linkedList.add(500);
+// linkedList.add(-10);
+// linkedList.add(-3);
+// linkedList.add(4);
+
+// const sorter = new Sorter(linkedList);
+// sorter.sort();
+// linkedList.print();
+
+const linkedList = new LinkedList();
+
+linkedList.add(500);
+linkedList.add(-10);
+linkedList.add(4);
+
+// const sorter = new Sorter(linkedList);
+sorter.sort();
+linkedList.print();
